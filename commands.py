ef096cbecc9488dcb37fb1798de54863e9b52eea
Diego del Corral <diegocorral.del@gmail.com>
2018-07-18 20:53:27 +0200

    Wallet, top 10, add coin answer with basic text

--- commands.py
+++ commands.py
@@ -7,8 +7,12 @@ def button(bot, update):
     query = update.callback_query
     if query.data == 'wallet':
         res = wallet()
+    elif query.data == "top_10":
+        res = "Top 10"
+    elif query.data == "add_coin":
+        res = "Add coin"
     else:
-        res = "default"
+        res = "Default"
     bot.send_message(chat_id=query.message.chat_id, text=res)
     bot.answer_callback_query(query.id)
 
